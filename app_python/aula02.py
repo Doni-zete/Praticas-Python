1ae501eb63ff83ca73fe38b220c06fee75d8609f
Doni-zete <donizete112@gmail.com>
2021-11-20 22:06:54 -0300

    aula02

---
+++ app_python/aula02.py
@@ -0,0 +1,10 @@
+a = int(input("Primeiro numero:"))
+b = int(input("Segundo numero:"))
+c = int(input("Terceiro numero:"))
+if a > b and a >c:
+    print("O maior numero e {}".format(a))
+elif b> a and b>c:
+    print("o maior numero e {}".format(b))
+else:
+    print("o maior numero e {}".format(c))
+    print ("final do programa")
