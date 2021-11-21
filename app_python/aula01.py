8bf8251356e3d863adf8cca4004adef16bd822a1
Doni-zete <donizete112@gmail.com>
2021-11-20 21:54:56 -0300

    aula01

---
+++ app_python/aula01.py
@@ -0,0 +1,18 @@
+a=10
+b=5
+soma =a +b
+sub =a-b
+mult =a*b
+div =a/b
+resto = a%b
+print(type(soma))
+print('soma '+str (soma))
+print(soma)
+print('sub': + str(sub))
+print(div)
+print(int(div))
+print(type(div))
+# x ='1'
+# soma2 = int(x)+1
+# print(mult)
+print(resto)
