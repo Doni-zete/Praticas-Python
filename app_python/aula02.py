5cb6493bbd1bc26bd22e62432cde812fe962910a
Doni-zete <donizete112@gmail.com>
2021-11-20 22:13:55 -0300

    aula02

--- app_python/aula02.py
+++ app_python/aula02.py
@@ -1,10 +1,19 @@
-a = int(input("Primeiro numero:"))
-b = int(input("Segundo numero:"))
-c = int(input("Terceiro numero:"))
-if a > b and a >c:
-    print("O maior numero e {}".format(a))
-elif b> a and b>c:
-    print("o maior numero e {}".format(b))
-else:
-    print("o maior numero e {}".format(c))
-    print ("final do programa")
+# a = int(input("Primeiro numero:"))
+# b = int(input("Segundo numero:"))
+# c = int(input("Terceiro numero:"))
+# if a > b and a >c:
+#     print("O maior numero e {}".format(a))
+# elif b> a and b>c:
+#     print("o maior numero e {}".format(b))
+# else:
+#     print("o maior numero e {}".format(c))
+#     print ("final do programa")
+
+# a= int (input("Entre com um valor: "))
+# b= int (input("Entre com o segundo valor: "))
+# resto_a = a%2
+# resto_b = b%2
+# if resto_a ==0 or not resto_b>0:
+#     print("numero e par")
+# else:
+#     print(("nenhum numero par digitado "))
