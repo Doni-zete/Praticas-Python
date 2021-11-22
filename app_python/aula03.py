71079e3d368a48c0b3aba455c8e999d7d9ca366d
Doni-zete <donizete112@gmail.com>
2021-11-21 22:43:14 -0300

    aula03

---
+++ app_python/aula03.py
@@ -0,0 +1,39 @@
+nota = int (input("Entre com a nota"))
+while nota>10:
+    nota = int(input("Nota invalida"))
+    print(nota)
+
+# a = 0
+# while a<=10:
+#     print(a)
+#     a += 1
+
+
+#
+#
+#
+#
+# a = int (input( "entre com um valor:"))
+# for num in range (a+1):
+#     div = 0
+#     for x in range (1, num+1):
+#         resto= num % x
+#         # print(a,resto)
+#         if resto == 0:
+#             div+=1
+#         if div ==2:
+#             print(num)
+
+
+# a = int(input("Entre com um numero: "))
+#
+# div = 0
+# for x in range (1,a+1):
+#     resto= a % x
+#     print(a,resto)
+#     if resto == 0:
+#         div+=1
+#     if div ==2:
+#         print("numero {} e primo ".format(a))
+#     else:
+#         print("numero {} nao e primo ".format(a))
