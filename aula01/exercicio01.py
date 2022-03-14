7213108d2c135a2c2a9a58bba0671b24ab16f193
Donizete Crisostomo <donizete112@gmail.com>
2022-03-13 22:00:52 -0300

    coversão dolar para reais

--- aula01/exercicio01.py
+++ aula01/exercicio01.py
@@ -16,9 +16,25 @@ print(f'Ok, seu nome é: {nome} e sua idade:{idade}') """
 # area = float(altura) * float(largura)
 # print(f'A area é: {area}')
 
-valorProduto = input("Entre com o valor do produto: ")
-valorDesconto = input("Entre com o desconto em porcentagem: ")
+# valorProduto = input("Entre com o valor do produto: ")
+# valorDesconto = input("Entre com o desconto em porcentagem: ")
 
-porcentagem = float(valorProduto)*(float(valorDesconto)/100)
-conta = float(valorProduto)-float(porcentagem)
-print(f'Valor com R${porcentagem} de desconto dá R$: {conta}')
+# porcentagem = float(valorProduto)*(float(valorDesconto)/100)
+# conta = float(valorProduto)-float(porcentagem)
+# print(f'Valor com R${porcentagem} de desconto dá R$: {conta}')
+
+
+# Conversão de reais para dolar
+
+reais = input("Entre com o valor em Reais: ")
+
+conversao = float(reais) / 5.07
+
+print(f"Valor de reais para dolar é: ${conversao}")
+
+
+dolar = input("Entre com o valor em Dolar: ")
+
+conversao = float(dolar) * 5.07
+
+print(f"Valor de dolar para reais é: ${conversao}")
