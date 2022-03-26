474e13ddc4fe778ea826393396661a6b1be8f04f
Donizete Crisostomo <donizete112@gmail.com>
2022-03-26 00:01:46 -0300

    exer2, while

--- aula01/exercicio02.py
+++ aula01/exercicio02.py
@@ -8,15 +8,27 @@ elif int(idade) > 18:
     print('MAIOR') """
 
 
-a = 8
+""" a = 8
 b = 3
-c = 1
+c = 1 """
 """ if a > b: print("b eh maior do que a")
 print("Codigo fora do bloco if") """
 
 # print("B") if b < a else print("A") # Operador ternario
 
-if a > b:
+""" if a > b:
     print("A")
     if a > c:
-        print("A eh maior que b e que c")
+        print("A eh maior que b e que c") """
+
+a = input("Entre com sua idade:")
+idade = int(a)
+
+if idade <= 7:
+    print("Infantil A")
+elif idade <= 10:
+    print("Infantil B")
+elif idade <= 13:
+    print("Juvenil A")
+elif idade <= 17:
+    print("juvenil B")
--- aula01/while.py
+++ aula01/while.py
@@ -1,8 +1,19 @@
 a = 0
 
-while a <= 5:
-    print(a)
+""" while a <= 5:
     print(a <= 5)
+    print(a)
+    if a == 3:
+        break
     a = a + 1
-print("Resultado final de a: ", a)    
+print("Resultado final de a: ", a)
 print(a <= 5)
+ """
+
+
+while a <= 5:
+    print(a <= 5)
+    print(a)
+    a = a + 1
+else:
+    print(f"Resultado final de a 5. VAlor de a  {a}")
