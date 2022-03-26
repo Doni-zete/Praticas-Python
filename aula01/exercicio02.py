5be55b933782a834c6f4b19b55e61b6ae1e392f3
Donizete Crisostomo <donizete112@gmail.com>
2022-03-25 23:11:44 -0300

    ternario

--- aula01/exercicio02.py
+++ aula01/exercicio02.py
@@ -1,4 +1,16 @@
-idade = input('Entre com sua idade: ')
+""" idade = input('Entre com sua idade: ')
 
-if idade < 16:
+if int(idade) < 16:
+    print('MENOR')
+elif int(idade) >= 16 & int(idade) <= 18:
+    print('EMANCIPADO')
+elif int(idade) > 18:
+    print('MAIOR') """
 
+
+a = 5
+b = 8
+""" if a > b: print("b eh maior do que a")
+print("Codigo fora do bloco if") """
+
+print("B") if b < a else print("A") # Operador ternario
