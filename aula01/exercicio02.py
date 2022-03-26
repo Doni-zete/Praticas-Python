44e32bdc401a7c1d7b1723ea6560d48f776d3bf6
Donizete Crisostomo <donizete112@gmail.com>
2022-03-25 23:17:36 -0300

    ternario

--- aula01/exercicio02.py
+++ aula01/exercicio02.py
@@ -8,9 +8,18 @@ elif int(idade) > 18:
     print('MAIOR') """
 
 
-a = 5
-b = 8
+a = 8
+b = 3
+c = 1
 """ if a > b: print("b eh maior do que a")
 print("Codigo fora do bloco if") """
 
-print("B") if b < a else print("A") # Operador ternario
+#print("B") if b < a else print("A") # Operador ternario
+
+if a > b:
+    print("A")
+    if a > c:
+        print("A eh maior que b e que c")
+
+
+        
