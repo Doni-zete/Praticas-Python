17b4e78a825f7b91dc833d8bcfb02948065c5c14
Donizete Crisostomo <donizete112@gmail.com>
2022-03-30 17:11:23 -0300

    adivinhar um numero

---
+++ aula01/dowhile.py
@@ -0,0 +1,18 @@
+""" 
+do while
+Codigo para adivinhar um numero
+ """
+
+palpite = 0
+numero = 9
+
+# while palpite != numero:
+print("Qual o numero correto?")
+# palpite = int(input())
+# print("Voce errou")
+print("Parabens voce acertou")
+
+print(bool(palpite))
+
+
+
