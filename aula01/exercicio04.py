42c2c95da33bf8a3f517415d1b095256b78ab111
Donizete Crisostomo <donizete112@gmail.com>
2022-03-30 16:43:46 -0300

    maior, menor

---
+++ aula01/exercicio04.py
@@ -0,0 +1,7 @@
+""" Maior, menor ou igaual a zero """
+numero = int(input("Digite um numero: "))
+
+if(numero > 0):
+    print(f"O {numero} MAIOR que zero")
+elif(numero < 0):
+    print(f"O {numero} MENOR que zero")
