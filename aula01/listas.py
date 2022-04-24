a378791a5b94115ffdcc2fca2a3f7e0a0295649e
Donizete Crisostomo <donizete112@gmail.com>
2022-04-24 16:49:11 -0300

    lista

---
+++ aula01/listas.py
@@ -0,0 +1,15 @@
+lista = ["chicago", "queens", "Ronaldo", "Listao"]
+print(lista)
+
+lista2 = [52, 2, 6, 9, 200]
+print(lista2)
+
+lista3 = [True, False]
+print(lista3)
+# index     0       1        2     3
+lista5 = [True, "chicago", 2.5, False, 4]
+print(lista5)
+
+print(type(lista3))
+
+print(lista5[4])
