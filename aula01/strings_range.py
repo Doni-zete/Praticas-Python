bf04b22c580cce4389c073ffa85706038ac10f18
Donizete Crisostomo <donizete112@gmail.com>
2022-04-24 16:17:04 -0300

    strings_range

---
+++ aula01/strings_range.py
@@ -0,0 +1,19 @@
+""" for x in range(100, 1, -5):
+    print(x) """
+
+""" nome = "chicago"
+nome2 = "quens"
+print(nome, end=" ")
+print(nome2) """
+
+""" nome = "chicago"
+for x in nome:
+    print(x, end="#")
+ """
+
+x = 5
+y = 0
+x, y = y, x
+
+print(x)
+print(y)
