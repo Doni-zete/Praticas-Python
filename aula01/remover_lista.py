8c9980d84c10a02e0ba23f7348d3a1f215de4aa7
Donizete Crisostomo <donizete112@gmail.com>
2022-05-08 23:05:02 -0300

    remove lista

---
+++ aula01/remover_lista.py
@@ -0,0 +1,24 @@
+""" lista = ["carro", "barco", "avião"]
+print(lista)
+
+lista.pop(0)
+
+print(lista) """
+
+""" carro = ["gol", "fiat", "palio", "camaro", "fusca"]
+carro.sort()  # ordenação
+# carro.clear() """
+
+
+""" lista = [50, 200, 98]
+print(lista)
+
+lista.sort(reverse=True)  # decrescente ordenada
+print(lista)
+ """
+
+lista = ["Maria", "joão", "Ronaldo"]
+print(lista)
+
+lista.sort(key=str.lower)
+print(lista)
