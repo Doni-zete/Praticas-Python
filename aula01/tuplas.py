36a877a9f22f9992550fb6e3bdb89c751d6299ef
Donizete Crisostomo <donizete112@gmail.com>
2022-05-09 20:28:46 -0300

    tupla

---
+++ aula01/tuplas.py
@@ -0,0 +1,72 @@
+""" lista = ["item1", "item2", "item3"]
+print(lista)
+print(len(lista))
+print(type(lista))
+print("*"*10)
+tuplas = ("item", "item2", "item3")
+print(tuplas)
+print(len(tuplas))
+print(type(tuplas))
+print(tuplas[2])
+
+print(tuplas.count("item1"))
+tuplas.append("item4")
+print(tuplas)
+
+lista = []
+lista.append("item1")
+print(lista)
+ """
+
+""" lista = ["item1", "item2"]
+print(lista)
+lista[0] = "item3"
+print(lista) """
+
+""" tupla = ("item1", "item2")
+print(tupla)
+tupla[0] = "item3"
+print(tupla)
+ """
+
+""" tupla = ("item1", "item2")
+print(tupla)
+
+tupla = ("item3", "item4", "verde")
+print(tupla) """
+
+
+""" unidades_federativas = ["SP", "DF", "GO"]
+
+print(type(unidades_federativas)) """
+""" 
+tupla = (3, 5.0, True, "item")
+print(tupla) """
+
+
+""" tupla = ("item1", "item2", "item3", "item4")
+lista = list(tupla)
+print(tupla)
+print(lista)
+
+lista.pop()
+print(lista)
+
+tupla = tuple(lista)
+del tupla
+
+print(tupla) """
+
+
+tupla = ("item1",)
+tupla2 = ("a", "b")
+
+tupla = "item1", "item2", "item3"
+print(tupla)
+
+
+for variavel in tupla:
+    print(variavel)
+
+for x in range(len(tupla)):
+    print(x)
