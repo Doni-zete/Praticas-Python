d23f915283a5708c2567bfe5a414f16a35d87ef3
Donizete Crisostomo <donizete112@gmail.com>
2022-06-04 17:23:05 -0300

    paciente

--- aula01/classe/paciente.py
+++ aula01/classe/paciente.py
@@ -1,7 +1,16 @@
-""" classe
-objeto
-construtor """
+""" 
+classe - Um conjunto de objetos com as memsa caracteristicas
+objeto -  Representação do mundo real como um tipo de dado de uma classe
+construtor - É uma função criada  implitamente com o mesmo nome da classe
+Atributo - São as variaveis de uma classe 
+"""
 
 
 class Paciente:
-  pass
+
+    def __init__(self, nome, idade, cpf, email):
+        print("ok")
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.email = email
