57491dec4c1355984669c2a25dad1e4a7d64b9f6
Donizete Crisostomo <donizete112@gmail.com>
2022-06-04 22:32:52 -0300

    herança simples

--- aula01/classe/escola.py
+++ aula01/classe/escola.py
@@ -1,12 +1,69 @@
-class Aluno:
+class Pessoa:
+  # superclasse - classe mãe, clase pai
 
-    def __init__(self, nome, idade, matricula):
-        self.__nome = nome
-        self.idade = idade
-        self.matricula = matricula
-        self.notas = None
+    def __init__(self, nome=None, data=None, cpf=None, rg=None):
+        self.nome = nome
+        self.data_nascimento = data
+        self.cpf = cpf
+        self.rg = rg
+        print("Classe PEssoa")
 
-# get e set
+    def imprimir_nome(self):
+        return self.nome
+
+
+class Aluno(Pessoa):
+    # subclasse - classe filha, classe filho
+
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.matricula = None
+        self.notas = []
+        print("Classe aluno")
+
+    def estudar(self):
+        return "Estudando..."
+
+
+class Professor(Pessoa):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+        self.disciplinas = []
+
+    def lecionar(self):
+        return "Ensinando..."
+
+
+aluno1 = Aluno('ana')
+print(aluno1.imprimir_nome())
+
+
+professor1 = Professor("MArcos")
+print(professor1.imprimir_nome())
+
+print(aluno1.estudar())
+print(professor1.lecionar())
+
+""" print(type(aluno1))
+print(type(professor1))
+
+
+  """
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" # get e set
     @property
     def nome(self):
         return self.__nome
@@ -33,6 +90,14 @@ print(aluno1.nome)
 print(aluno1.idade)
 aluno1.idade = 23
 print(aluno1.idade)
+
+ """
+
+
+
+
+
+
 
 
 
