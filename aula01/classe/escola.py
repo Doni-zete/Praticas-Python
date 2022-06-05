1bc1dd3d28b7f221cffb4a63572602a54655fd42
Donizete Crisostomo <donizete112@gmail.com>
2022-06-04 21:18:08 -0300

    quadrado

--- aula01/classe/escola.py
+++ aula01/classe/escola.py
@@ -1,2 +1,19 @@
-class Pessoa:
-  pass
+class Quadrado:
+
+  def __init__(self, lado):
+      self.__lado = lado
+      self.__area = lado * lado
+
+  def retorna_area(self):
+      print(self.__area)   
+
+quadrado = Quadrado(2)         
+quadrado.retorna_area()  
+quadrado.area = 7
+quadrado.retorna_area()
+print(quadrado.__dict__)
+quadrado._Quadrado__area = 13
+quadrado.retorna_area()  
+
+
+
